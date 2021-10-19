5a48309a22a14b62e34bfec9818cc6b8334f6f00
Ashni Damaria <72804981+ashnidamaria@users.noreply.github.com>
2021-10-19 22:57:40 +0530

    Update fresh_app.py

--- fresh_app.py
+++ fresh_app.py
@@ -32,4 +32,4 @@ async def predict(image_url: PredictApiRequest):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
